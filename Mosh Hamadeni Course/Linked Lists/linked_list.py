e15763b2824943a752d875030907f903185f81ba
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-04-13 12:11:26 +0100

    Linked List -> remove_last

--- Mosh Hamadeni Course/Linked Lists/linked_list.py
+++ Mosh Hamadeni Course/Linked Lists/linked_list.py
@@ -14,7 +14,7 @@ class LinkedList():
     self.node = self.Node(item)
     self.length += 1
 
-    if self.is_empty():
+    if self._is_empty():
       self.first = self.last = self.node
     else:
       self.last.next_node = self.node
@@ -24,13 +24,13 @@ class LinkedList():
     self.node = self.Node(item)
     self.length += 1
 
-    if self.is_empty():
+    if self._is_empty():
       self.first = self.last = self.node
     else:
       self.node.next_node = self.first
       self.first = self.node
   
-  def is_empty(self):
+  def _is_empty(self):
     return self.first == None
 
   def index_of(self, item):
@@ -48,7 +48,7 @@ class LinkedList():
     return self.index_of(item) != -1
 
   def remove_first(self):
-    if self.is_empty():
+    if self._is_empty():
       raise Exception('Linked list is Empty')
     elif self.first == self.last:
       self.first = self.last = None
@@ -59,30 +59,41 @@ class LinkedList():
       self.first = self.second
       self.length -= 1
 
-
-
-
-
-
-    if self.first and self.first.next_node:
-      self.second = self.first.next_node
-      self.first.next_node = None
-      self.first = self.second
+  def remove_last(self):
+    if self._is_empty():
+      raise Exception('Linked-List is Empty.')
+    elif self.first == self.last:
+      self.first = self.last = None
+      self.length -= 1
+    else:
+      self.pervious = self._get_previous(self.last)
+      self.last = self.pervious
+      self.last.next_node = None
       self.length -= 1
 
+  def _get_previous(self, node):
+    self.current = self.first
+    while self.current != None:
+      if self.current.next_node == node:
+        return self.current
+      else:
+        self.current = self.current.next_node
+    
 
+    
 
 
 linked_list = LinkedList()
 linked_list.add_last(10)
 linked_list.add_last(20)
-# linked_list.add_last(30)
+linked_list.add_last(30)
 # linked_list.add_first(50)
-linked_list.remove_first()
+# linked_list.remove_first()
+linked_list.remove_last()
 
 print(linked_list.first.value)
 # print(linked_list.first.next_node.value)
-# print(linked_list.last.value)
+print(linked_list.last.value)
 # print(linked_list.last.next_node)
 print(linked_list.length)
 # print(linked_list.index_of(100))

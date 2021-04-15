b5d570622a6e8f4757025311b509a1603ff3ed2e
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-04-15 12:33:35 +0100

    Stacks -> balanced_string Final

--- Mosh Hamadeni Course/Stacks/balanced_strings.py
+++ Mosh Hamadeni Course/Stacks/balanced_strings.py
@@ -4,17 +4,27 @@ class Expression():
     self.string = string
 
   stack = []
+  lef_brackets = '([{<'
+  right_brackets = ')]}>' 
 
   def is_balanced(self):
-    for i in self.string:
-      if i == '(':
-        self.stack.append(i)
-      elif i == ')':
+    for ch in self.string:
+      if ch in self.lef_brackets:
+        self.stack.append(ch)
+
+      elif ch in self.right_brackets:
         if not self.stack: return False
-        self.stack.pop()
+
+        top = self.stack.pop()
+        if not self._brackets_match(ch, top):
+          return False
+
     return not bool(self.stack)
 
-expression = Expression('(1 + 2)')
+  def _brackets_match(self, right, left):
+    return self.lef_brackets.index(left) == self.right_brackets.index(right)
+      
+expression = Expression('(1 + 2) <tag>')
 
 print(expression.is_balanced())
 

e834d88d04033fc7d2a17d5a666e11c6d8e49c8d
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-04-19 10:00:40 +0100

    Queues -> Practice

--- Mosh Hamadeni Course/Queues/queues_practice.py
+++ Mosh Hamadeni Course/Queues/queues_practice.py
@@ -1,7 +1,7 @@
 # Reversing a queue using a stack.
 queue = [10, 20, 30]
 stack = []
-def reverse_queue(queue):
+def reverse_queue(item):
   while queue:
     stack.append(queue.pop(0))
   while stack:
@@ -14,59 +14,55 @@ def reverse_queue(queue):
 class ArrayQueue():
   def __init__(self, capacity):
     self.capacity = capacity
-    self.queue = [0 for i in range(self.capacity)]
-  
-  count = 0
+    self.queue = [0 for i in range(capacity)]
+
   front = 0
   rear = 0
-
-  def is_full(self):
-    if self.count == self.capacity: return True
-    return False
+  count = 0
 
   def is_empty(self):
     if self.count == 0: return True
     return False
 
+  def is_full(self):
+    if self.count == self.capacity: return True
+    return False
+  
   def enqueue(self, item):
-    if self.is_full(): raise Exception('Queue is Full!')
+    if self.is_full(): raise Exception('Queue is Full.')
 
     self.queue[self.rear] = item
-    self.count += 1
     self.rear = (self.rear + 1) % self.capacity
+    self.count += 1
 
   def dequeue(self):
-    if self.is_empty(): raise Exception('Queue is Empty!')
+    if self.is_empty(): raise Exception('Queue is empty.')
 
     item = self.queue[self.front]
     self.queue[self.front] = 0
     self.front = (self.front + 1) % self.capacity
     self.count -= 1
+
     return item
-  
-  def peek(self, index):
-    while index > self.front and index < self.rear:
-      return queue[index]
+
+  def peek(self):
+    return f'The front of the queue contains {self.queue[self.front]} and the rear contains {self.queue[self.rear -1]}'
 
 test_queue = ArrayQueue(5)
 test_queue.enqueue(10)
 test_queue.enqueue(20)
 test_queue.enqueue(30)
-test_queue.dequeue()
-front = test_queue.dequeue()
 test_queue.enqueue(40)
 test_queue.enqueue(50)
+test_queue.dequeue()
+test_queue.dequeue()
+test_queue.dequeue()
 test_queue.enqueue(60)
 test_queue.enqueue(70)
-test_queue.dequeue()
-print(front)
+test_queue.enqueue(80)
+
+
+item = test_queue.dequeue()
+print(item)
 print(test_queue.queue)
-# print(test_queue.dequeue())
-# print(test_queue.dequeue())
-# print()
-
-# print(test_queue.count)
-# print(test_queue.rear)
-# print(test_queue.front)
-
-
+print(test_queue.peek())

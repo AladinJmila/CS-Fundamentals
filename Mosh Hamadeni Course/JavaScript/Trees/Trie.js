e52091f6d28873bb3ce9764849c16641a544a880
AladinJmila <jmila.aladin@gmail.com>
2021-12-02 13:24:04 +0000

    Tries completed

--- Mosh Hamadeni Course/JavaScript/Trees/Trie.js
+++ Mosh Hamadeni Course/JavaScript/Trees/Trie.js
@@ -60,8 +60,81 @@ class TrieHash {
     }
     current.isEndOfWord = true;
   };
+
+  contains(word) {
+    let current = this.root;
+    for (let ch of word) {
+      if (!current.children[ch]) return false;
+      current = current.children[ch];
+    }
+    return current.isEndOfWord;
+  }
+
+  traverse(root = this.root) {
+    for (let child of Object.values(root.children)) {
+      this.traverse(child);
+    }
+    console.log(root.value);
+  }
+
+  remove(word, root = this.root, index = 0) {
+    if (index === word.length) {
+      root.isEndOfWord = false;
+      return;
+    }
+
+    let ch = word[index];
+    let child = root.children[ch];
+    if (!child) return;
+
+    this.remove(word, child, index + 1);
+
+    if (!this.hasChildren(child) && !child.isEndOfWord) {
+      delete root.children[ch];
+    }
+  }
+
+  hasChildren(node) {
+    return Object.keys(node.children).length !== 0;
+  }
+
+  findWords(prefix) {
+    let lastNode = this.findLastNodeOf(prefix);
+    let words = [];
+    this.findWordsRec(lastNode, prefix, words);
+
+    return words;
+  }
+
+  findWordsRec(root, prefix, words) {
+    if (!root) return;
+
+    if (root.isEndOfWord) {
+      words.push(prefix);
+    }
+
+    for (let child of Object.values(root.children)) {
+      this.findWordsRec(child, prefix + child.value, words);
+    }
+  }
+
+  findLastNodeOf(prefix) {
+    if (!prefix) return null;
+
+    let current = this.root;
+    for (let ch of prefix) {
+      let child = current.children[ch];
+      if (!child) return null;
+      current = child;
+    }
+    return current;
+  }
 }
 
 const trie = new TrieHash();
-trie.insertMosh('cat');
-trie.insertMosh('calf');
+trie.insertMosh('car');
+trie.insertMosh('card');
+trie.insertMosh('care');
+trie.insertMosh('careful');
+trie.insertMosh('egg');
+console.log(trie.findWords());

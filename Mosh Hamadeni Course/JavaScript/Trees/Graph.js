4defea2c68efd1d6cb6385d4b3900a5a78505c10
AladinJmila <jmila.aladin@gmail.com>
2021-12-06 12:57:13 +0000

    Added removeNode and removeEdge operations

--- Mosh Hamadeni Course/JavaScript/Trees/Graph.js
+++ Mosh Hamadeni Course/JavaScript/Trees/Graph.js
@@ -11,17 +11,15 @@ class Graph {
   addNode = label => {
     const node = new this.Node(label);
     !this.nodes[label] && (this.nodes[label] = node);
-    !this.adjacencyList[node] && (this.adjacencyList[node.label] = []);
+    !this.adjacencyList[node.label] && (this.adjacencyList[node.label] = []);
   };
 
   addEdge = (from, to) => {
-    const fromNode = this.nodes[from];
-    if (!fromNode) throw new Error('Illegal Argument');
+    if (!this.nodes[from]) throw new Error('Illegal Argument');
 
-    const toNode = this.nodes[to];
-    if (!toNode) throw new Error('Illegal Argument');
+    if (!this.nodes[to]) throw new Error('Illegal Argument');
 
-    this.adjacencyList[fromNode.label].push(toNode.label);
+    this.adjacencyList[from].push(to);
   };
 
   print = () => {
@@ -32,13 +30,37 @@ class Graph {
       }
     }
   };
+
+  removeNode = label => {
+    if (!this.nodes[label]) return;
+
+    for (let value of Object.values(this.adjacencyList)) {
+      let index = value.indexOf(label);
+      if (index !== -1) {
+        value.splice(index, 1);
+      }
+    }
+
+    delete this.adjacencyList[label];
+    delete this.nodes[label];
+  };
+
+  removeEdge = (from, to) => {
+    if (!this.nodes[from] || !this.nodes[to]) return;
+
+    const index = this.adjacencyList[from].indexOf(to);
+    if (index !== -1) {
+      this.adjacencyList[from].splice(index, 1);
+    }
+  };
 }
 
 const graph = new Graph();
-graph.addNode('aladin');
-graph.addNode('josipa');
-graph.addNode('buba');
-graph.addEdge('aladin', 'josipa');
-graph.addEdge('aladin', 'buba');
-graph.addEdge('josipa', 'buba');
+graph.addNode('A');
+graph.addNode('B');
+graph.addNode('C');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.removeNode('A');
+graph.addEdge('B', 'C');
 graph.print();

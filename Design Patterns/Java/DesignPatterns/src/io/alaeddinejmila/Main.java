1aba86b800fb001ed0e1646c4cc70d6350e79485
AladinJmila <jmila.aladin@gmail.com>
2021-12-21 18:09:36 +0000

    momento pattern

--- Design Patterns/Java/DesignPatterns/src/io/alaeddinejmila/Main.java
+++ Design Patterns/Java/DesignPatterns/src/io/alaeddinejmila/Main.java
@@ -1,15 +1,27 @@
 package io.alaeddinejmila;
 
+import io.alaeddinejmila.memento.Editor;
+import io.alaeddinejmila.memento.History;
+
 public class Main {
 
     public static void main(String[] args) {
-        drawUIControl(new TextBox());
-        drawUIControl(new CheckBox());
+        var editor = new Editor();
+        var history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
     }
 
-    public static void drawUIControl(UIControl control) {
-        control.draw();
-    }
 
 }
 

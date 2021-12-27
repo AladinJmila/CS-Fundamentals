2b5d4ddc046f714a0ddb59ee10ce6f235d1cbec7
AladinJmila <jmila.aladin@gmail.com>
2021-12-27 13:54:17 +0000

    itearator solution with array

--- Design Patterns/Java/DesignPatterns/src/io/alaeddinejmila/iterator/BrowseHistory.java
+++ Design Patterns/Java/DesignPatterns/src/io/alaeddinejmila/iterator/BrowseHistory.java
@@ -2,47 +2,45 @@ package io.alaeddinejmila.iterator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class BrowseHistory {
-    private List<String> urls = new ArrayList<>();
+    private String[] urls = new String[10];
+    private int count;
 
     public void push(String url) {
-        urls.add(url);
+        urls[count++] = url;
     }
 
     public String pop() {
-        var lastIndex = urls.size() - 1;
-        var lastUrl = urls.get(lastIndex);
-        urls.remove(lastUrl);
-
-        return lastUrl;
+        return urls[--count];
     }
 
     public Iterator<String> createIterator() {
-        return new ListIterator(this);
+        return new ArrayIterator(this);
     }
 
-    public class ListIterator implements Iterator<String> {
-        private BrowseHistory history;
-        private int index;
+   public class ArrayIterator implements Iterator<String> {
+       private BrowseHistory history;
+       private int index;
 
-        public ListIterator(BrowseHistory history) {
-            this.history = history;
-        }
+       public ArrayIterator(BrowseHistory history) {
+           this.history = history;
+       }
 
-        @Override
-        public boolean hasNext() {
-            return (index < history.urls.size());
-        }
+       @Override
+       public boolean hasNext() {
+           return (index < history.count);
+       }
 
-        @Override
-        public String current() {
-            return history.urls.get(index);
-        }
+       @Override
+       public String current() {
+           return history.urls[index];
+       }
 
-        @Override
-        public void next() {
+       @Override
+       public void next() {
             index++;
-        }
-    }
+       }
+   }
 }

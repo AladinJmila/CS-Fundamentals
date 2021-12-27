7b2d8a2c64a27f594e72d0620e5d2f9ae5e87407
AladinJmila <jmila.aladin@gmail.com>
2021-12-27 10:08:36 +0000

    Boilerplate for state pattern

---
+++ Design Patterns/Java/DesignPatterns/src/io/alaeddinejmila/state/Tool.java
@@ -0,0 +1,6 @@
+package io.alaeddinejmila.state;
+
+public interface Tool {
+    void mouseDown();
+    void mouseUp();
+}
---
+++ Design Patterns/Java/DesignPatterns/src/io/alaeddinejmila/state/ToolType.java
@@ -0,0 +1,7 @@
+package io.alaeddinejmila.state;
+
+public enum ToolType {
+    SELECTION,
+    BRUSH,
+    ERASER
+}
